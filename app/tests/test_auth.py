413c045da50f5431816fc014a925eaa7ddc2bde9
Lucas Stofaleti <lucastofaleti@hotmail.com>
2023-04-22 14:39:38 +0200

    feat: hyperlink signup/login

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -2,6 +2,7 @@ from fastapi.testclient import TestClient
 import mongomock
 from app.main import app
 from app.db.database import get_connection
+from app.utils.auth import *
 
 mongo = mongomock.MongoClient()
 db = mongo.get_database("hireme")
@@ -14,12 +15,18 @@ def connection_override():
 
 app.dependency_overrides[get_connection] = connection_override
 
+# Utils
+def test_password_hash():
+    password = "mypassword"
+    hashed_password = get_password_hash(password)
+    check_passwords = verify_password(password, hashed_password)
+    assert check_passwords == True
+
+# Endpoints
 def test_signup_get():
     response = client.get("/signup")
     assert response.status_code == 200
 
 def test_signup_post_without_inputs():
     response = client.post("/signup")
-    print(response.json())
-    print(response.content)
-    assert response.status_code == 200
+    assert response.status_code == 422
